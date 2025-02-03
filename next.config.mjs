1f7bb717c975739015946fd556b99601eaf40754
MisterPingouin <anthony.axeldisplay@gmail.com>
2025-02-03 21:10:28 +0100

    no eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        // Ne pas échouer le build en cas d’erreurs ESLint
+        ignoreDuringBuilds: true,
+      },
 };
 
 export default nextConfig;
