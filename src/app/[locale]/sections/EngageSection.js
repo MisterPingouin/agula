c70192e5be35597fb132dd77ff5767754473346c
MisterPingouin <anthony.axeldisplay@gmail.com>
2025-02-04 02:56:45 +0100

    engagesection desktop

--- src/app/[locale]/sections/EngageSection.js
+++ src/app/[locale]/sections/EngageSection.js
@@ -5,10 +5,9 @@ const t = useScopedI18n("engage");
 
 
   return (
-    <section className="flex flex-col px-6 pt-6">
-    <p className="font-bold font-title text-25px text-green">{t("title")}</p>
+    <section className="flex flex-col md:flex-row justify-center md:w-[1240px] md:mx-auto md:gap-10 px-6 pt-6">
+    <p className="font-bold font-title text-25px text-green text-nowrap">{t("title")}</p>
     <p className="font-content text-15px mt-2 pb-2">{t("content")}</p>
-
     </section>
   );
 };
