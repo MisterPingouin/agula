069dd11ae25f36dde0e3df84177fc8fd2f43b96a
MisterPingouin <anthony.axeldisplay@gmail.com>
2025-02-11 17:50:18 +0100

    small fix

--- src/app/[locale]/components/Header/Header.js
+++ src/app/[locale]/components/Header/Header.js
@@ -109,10 +109,10 @@ const HeaderMobile = ({ t, menuOpen, toggleMenu }) => (
         <Link href="https://www.tripadvisor.fr/Attraction_Review-g663644-d25436276-Reviews-L_Agula_Marina_Croisieres-Cargese_Corse_du_Sud_Corsica.html">
           <Image src="/images/tripheader.svg" alt="Tripadvisor" width={26} height={26} />
         </Link>
-        <Link href="https://www.facebook.com/lagulamarinacroisieres/">
+        <Link href="https://www.instagram.com/lagulamarina_croisieres/">
           <Image src="/images/instaheader.svg" alt="Instagram" width={26} height={26} />
         </Link>
-        <Link href="https://www.instagram.com/lagulamarina_croisieres/">
+        <Link href="https://www.facebook.com/lagulamarinacroisieres/">
           <Image src="/images/fbheader.svg" alt="Facebook" width={26} height={26} />
         </Link>
       </div>
