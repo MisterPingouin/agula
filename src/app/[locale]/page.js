fefca3b394bbcac90708dd9e41854b7aaddb0821
MisterPingouin <anthony.axeldisplay@gmail.com>
2025-03-20 14:23:26 +0100

    last fix

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -16,9 +16,7 @@ import VideoLagula from "./components/VideoLagula";
 import Link from "next/link";
 import Image from "next/image";
 
-
 export default function HomePage() {
-
   return (
     <main>
       <HomeSection />
@@ -26,10 +24,25 @@ export default function HomePage() {
       <SliderDesktop />
       <section className="flex flex-col mt-8 px-4 gap-4 max-w-md mx-auto lg:hidden">
         <Card tourKey="grandTour" imageSrc="/images/grandtour.webp" />
-        <Card tourKey="calanquesPiana" imageSrc="/images/calanques-piana.webp" />
+        <Card
+          tourKey="calanquesPiana"
+          imageSrc="/images/calanques-piana.webp"
+        />
         <Card
           tourKey="scandolaGirolata"
           imageSrc="/images/scandola-girolata.webp"
+        />
+        <Card
+          tourKey="grandTourSunset"
+          imageSrc="/images/grandtour-sunset.webp"
+        />
+        <Card
+          tourKey="calanquesPianaSunset"
+          imageSrc="/images/calanques-piana-sunset.webp"
+        />
+        <Card
+          tourKey="scandolaGirolataSunset"
+          imageSrc="/images/scandola-girolata-sunset.webp"
         />
       </section>
       <EmbarkSection />
@@ -42,21 +55,21 @@ export default function HomePage() {
       <HeartDesktop />
       <FAQ />
       <Link href="https://www.google.com/maps/place/L'Agula+Marina+Croisi%C3%A8res+Promenade+en+mer+Carg%C3%A8se/@42.1317841,8.5939425,17z/data=!3m1!4b1!4m6!3m5!1s0x12da7bbb055319c5:0x5e3bb5ec35aeb829!8m2!3d42.1317841!4d8.5965174!16s%2Fg%2F11twhtgl6d?entry=ttu&g_ep=EgoyMDI1MDIyMy4xIKXMDSoASAFQAw%3D%3D">
-      <Image
+        <Image
           src="/images/map.webp"
           alt="Map"
           width={429}
           height={371}
           className="block lg:hidden h-auto w-full mt-4"
         />
-              <Image
+        <Image
           src="/images/mapdesktop.webp"
           alt="Map"
           width={1280}
           height={368}
           className="hidden lg:block h-auto w-full mt-4"
         />
-        </Link>
+      </Link>
     </main>
   );
 }
